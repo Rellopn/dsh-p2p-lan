4ccc33e14121332db9657b2c43c4f8bffe61f81f
Rellopn <614046330@qq.com>
2026-08-18 19:18:21 +0800

    chore: remove temp tail script

--- .tmp-wsl-tail.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "=== p2p-lan.log tail (last 15) ==="
-tail -15 /home/rellopn/.dsh/p2p-lan.log
-echo "=== last modified ==="
-stat -c '%y' /home/rellopn/.dsh/p2p-lan.log 2>/dev/null
