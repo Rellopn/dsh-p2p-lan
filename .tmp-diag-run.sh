66a3bb4fc70ae5d207ff3a19249a6ebced59f57b
Rellopn <614046330@qq.com>
2026-08-18 19:06:31 +0800

    chore: remove temporary WSL diagnostic scripts

--- .tmp-diag-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export PATH="/home/rellopn/.local/share/fnm/node-versions/v24.3.0/installation/bin:$PATH"
-node /mnt/e/code/dsh-p2p-lan/.tmp-diag-send.mjs
-sleep 3
-echo "=== web.log after diag ==="
-tail -25 /home/rellopn/.dsh/web.log
